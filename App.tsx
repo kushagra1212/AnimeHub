2dbd983b394fc7553438dc34a1db2ecb1cc6e868
kushagra1212 <kushagrarathore002@gmail.com>
2023-07-04 18:11:22 +0530

    update: new screen

--- App.tsx
+++ App.tsx
@@ -4,18 +4,42 @@ import { LogBox, StyleSheet, Text, View } from 'react-native';
 import TabNavigator from './src/Navigation';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 LogBox.ignoreLogs([
   'Cache data may be lost when replacing the Page field of a Query object.',
 ]);
 
+SplashScreen.preventAutoHideAsync();
 const client = new ApolloClient({
   uri: 'https://graphql.anilist.co',
   cache: new InMemoryCache(),
 });
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'bold-itatic': require('./assets/fonts/JetBrainsMono-BoldItalic.ttf'),
+    medium: require('./assets/fonts/JetBrainsMono-Medium.ttf'),
+    'semi-bold': require('./assets/fonts/JetBrainsMono-SemiBold.ttf'),
+    regular: require('./assets/fonts/JetBrainsMonoNL-Regular.ttf'),
+    thin: require('./assets/fonts/JetBrainsMonoNL-Thin.ttf'),
+    'extra-light': require('./assets/fonts/JetBrainsMonoNL-ExtraLight.ttf'),
+    'extra-bold': require('./assets/fonts/JetBrainsMonoNL-ExtraBold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <StatusBar style="auto" />
 
       <ApolloProvider client={client}>
@@ -23,7 +47,7 @@ export default function App() {
           <TabNavigator />
         </NavigationContainer>
       </ApolloProvider>
-    </SafeAreaView>
+    </View>
   );
 }
 
--- src/screens/News/AnimeNewsFeedScreen.tsx
+++ src/screens/News/AnimeNewsFeedScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Shadow,
   Fill,
@@ -6,15 +6,22 @@ import {
   Canvas,
   Group,
   SkiaView,
-  Skia,
   useDrawCallback,
   useCanvasRef,
   Circle,
-  Text as SkiaText,
   rrect,
   rect,
   vec,
   Vertices,
+  useImage,
+  Skia,
+  ImageSVG,
+  useSVG,
+  Image as SkiaImage,
+  Text as SkiaText,
+  useFont,
+  Glyphs,
+  SkFont,
 } from '@shopify/react-native-skia';
 import {
   View,
@@ -26,6 +33,9 @@ import {
   Modal,
   Text,
   Dimensions,
+  ScrollView,
+  ActivityIndicator,
+  TouchableHighlight,
 } from 'react-native';
 import { useQuery, gql } from '@apollo/client';
 import RNPickerSelect from 'react-native-picker-select';
@@ -33,7 +43,7 @@ import RenderHTML from 'react-native-render-html';
 import { AnimeNewsData, AnimeNewsVariables, Media } from '../../types';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { NewsStackParamList } from '../../Navigation';
-import { COLORS } from '../../theme';
+import { COLORS, SIZES } from '../../theme';
 import {
   genreOptions,
   sortOptions,
@@ -42,6 +52,8 @@ import {
 } from '../../utils';
 
 import { Entypo, Feather, FontAwesome } from '@expo/vector-icons';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 const GET_ANIME_NEWS = gql`
   query GetAnimeNews(
     $genre: String
@@ -109,21 +121,47 @@ const Neumorphism = () => {
   );
 };
 const RoundedNeumorphicButton = () => {
+  let svg = useSVG(require('../../../assets/filter.svg'));
+  const canvasWidth = 100;
+  const canvasHeight = 100;
+  const svgWidth = 80;
+  const svgHeight = 80;
+  const roundedRectWidth = 70;
+  const roundedRectHeight = 70;
   return (
-    <Canvas style={{ width: 256, height: 256 }}>
+    <Canvas
+      style={{
+        width: canvasWidth,
+        height: canvasHeight,
+        position: 'absolute',
+        top: 50,
+        right: 0,
+        padding: 0,
+        borderRadius: 30,
+      }}
+    >
       <RoundedRect
-        x={32}
-        y={32}
-        width={192}
-        height={192}
+        width={roundedRectWidth}
+        height={roundedRectHeight}
         r={50}
+        x={canvasWidth / 2 - roundedRectWidth / 2}
+        y={canvasHeight / 2 - roundedRectHeight / 2}
         color="#2F353A"
       >
-        <Shadow dx={2} dy={2} blur={2} color="#2F353A" inner />
-        <Shadow dx={-2} dy={-2} blur={50} color="#7777" inner />
-        <Shadow dx={-12} dy={-12} blur={20} color="#9999" />
-        <Shadow dx={2} dy={2} blur={2} color="#0000" />
+        <Shadow dx={-3} dy={-3} blur={3} color="#2F353A" inner />
+        <Shadow dx={-4} dy={-4} blur={10} color="#666666" />
+        <Shadow dx={5} dy={5} blur={5} color="#222222" />
+        <Shadow dx={3} dy={3} blur={2} color="#1C1F22" inner />
       </RoundedRect>
+      {svg && (
+        <ImageSVG
+          svg={svg}
+          height={svgWidth}
+          width={svgHeight}
+          x={canvasWidth / 2 - 15}
+          y={canvasHeight / 2 - 15}
+        />
+      )}
     </Canvas>
   );
 };
@@ -131,10 +169,74 @@ const RoundedNeumorphicButton = () => {
 interface AnimeNewsFeedScreenProps {
   navigation: NativeStackNavigationProp<NewsStackParamList>;
 }
-
+const Genre = ({ genre }: { genre: string }) => {
+  const canvasWidth = genre.length * 15;
+  const canvasHeight = 120;
+  const roundedRectWidth = canvasWidth - 10;
+  const roundedRectHeight = canvasHeight - 80;
+  return (
+    <Canvas
+      style={{
+        height: canvasHeight,
+        width: canvasWidth,
+        position: 'relative',
+        marginRight: 10,
+        marginTop: -canvasHeight / 3,
+      }}
+    >
+      <RoundedRect
+        width={roundedRectWidth}
+        height={roundedRectHeight}
+        r={52}
+        x={canvasWidth / 2 - roundedRectWidth / 2}
+        y={canvasHeight / 2 - roundedRectHeight / 2}
+        color="#222222"
+      >
+        <Shadow dx={2} dy={2} blur={1} color="#000000" inner />
+        <Shadow dx={5} dy={5} blur={5} color="#444444" inner />
+        <Shadow dx={-5} dy={-5} blur={10} color="#777777" />
+        <Shadow dx={-4} dy={-4} blur={5} color="#000000" inner />
+      </RoundedRect>
+    </Canvas>
+  );
+};
+const ReadMoreButton = () => {
+  const canvasWidth = 120;
+  const canvasHeight = 120;
+  const roundedRectWidth = canvasWidth - 10;
+  const roundedRectHeight = canvasHeight - 80;
+  return (
+    <Canvas
+      style={{
+        height: canvasHeight,
+        width: canvasWidth,
+        position: 'relative',
+        marginRight: 10,
+        marginTop: -canvasHeight / 3,
+      }}
+    >
+      <RoundedRect
+        width={roundedRectWidth}
+        height={roundedRectHeight}
+        r={52}
+        x={canvasWidth / 2 - roundedRectWidth / 2}
+        y={canvasHeight / 2 - roundedRectHeight / 2}
+        color="#9F33FC"
+      >
+        <Shadow dx={3} dy={3} blur={0} color="#8E2DE2" inner />
+        <Shadow dx={-3} dy={-3} blur={0} color="#5602C8" inner />
+        {/* <Shadow dx={5} dy={5} blur={5} color="#444444" inner />
+        <Shadow dx={-5} dy={-5} blur={10} color="#8E2DE2" />
+        <Shadow dx={-4} dy={-4} blur={5} color="#8E2DE2" inner /> */}
+      </RoundedRect>
+    </Canvas>
+  );
+};
 const AnimeNewsFeedScreen: React.FC<AnimeNewsFeedScreenProps> = ({
   navigation,
 }) => {
+  const svg = useSVG(require('../../../assets/img.svg'));
+
   const [modalVisible, setModalVisible] = useState(false);
 
   const [selectedGenre, setSelectedGenre] = useState('All');
@@ -147,6 +249,20 @@ const AnimeNewsFeedScreen: React.FC<AnimeNewsFeedScreenProps> = ({
   const [page, setPage] = useState(1);
   const perPage = 10;
   const ref = useCanvasRef();
+  const fontSize = 20;
+  const fontRegular = useFont(
+    require('../../../assets/fonts/JetBrainsMonoNL-Regular.ttf'),
+    fontSize
+  );
+
+  const fontMedium = useFont(
+    require('../../../assets/fonts/JetBrainsMono-Medium.ttf'),
+    fontSize
+  );
+  const fontExtraBold = useFont(
+    require('../../../assets/fonts/JetBrainsMonoNL-ExtraBold.ttf'),
+    25
+  );
   const { loading, error, data, fetchMore } = useQuery<
     AnimeNewsData,
     AnimeNewsVariables
@@ -214,7 +330,7 @@ const AnimeNewsFeedScreen: React.FC<AnimeNewsFeedScreenProps> = ({
     vec(width, height),
     vec(0, height),
   ];
-  const colors = ['#353A40', '#353A40', '#16171B', '#16171B'];
+  const colors = ['#424750', '#424750', '#202326', '#202326'];
   const indices = [0, 1, 2, 0, 3, 2];
   const handleLoadMore = () => {
     if (pageInfo?.hasNextPage) {
@@ -244,24 +360,114 @@ const AnimeNewsFeedScreen: React.FC<AnimeNewsFeedScreenProps> = ({
       description = `${description?.slice(0, 150)}...`;
     }
 
+    const canvasWidth = width;
+    const canvasHeight = 400;
+    const roundedRectWidth = canvasWidth - 30;
+    const roundedRectHeight = canvasHeight - 30;
+    const source = item.source === 'ORIGINAL' ? 'Verified' : 'Manga';
     return (
-      <TouchableOpacity onPress={() => handleNewsItemPress(item)}>
-        <View style={styles.card}>
+      <TouchableOpacity
+        style={{
+          width: canvasWidth,
+          height: canvasHeight,
+          marginVertical: 30,
+        }}
+        onPress={() => handleNewsItemPress(item)}
+      >
+        <Canvas
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <RoundedRect
+            width={roundedRectWidth}
+            height={roundedRectHeight}
+            r={50}
+            x={canvasWidth / 2 - roundedRectWidth / 2}
+            y={canvasHeight / 2 - roundedRectHeight / 2}
+            color="#2E3237"
+          >
+            {/* 
+              For the later use
+            <Shadow dx={10} dy={10} blur={10} color="#444444" inner />
+            <Shadow dx={-1} dy={-1} blur={10} color="#222222" inner /> */}
+            <Shadow dx={1} dy={1} blur={1} color="#666666" inner />
+            <Shadow dx={5} dy={5} blur={4} color="#000000" />
+          </RoundedRect>
+        </Canvas>
+        <View
+          style={[
+            styles.card,
+            {
+              position: 'absolute',
+              width: roundedRectWidth,
+              height: roundedRectHeight,
+              top: canvasWidth / 2 - roundedRectWidth / 2,
+              left: 10,
+              backgroundColor: 'transparent',
+            },
+          ]}
+        >
           <Text style={styles.title}>{item.title.english}</Text>
+
           <RenderHTML
             contentWidth={200}
+            baseStyle={{
+              fontSize: 15,
+              color: COLORS.GraySecondary,
+              width: '100%',
+              padding: 10,
+              textAlign: 'justify',
+            }}
             source={{ html: item.description?.slice(0, 150) }}
           />
-          {/* Render HTML content */}
           {description && description?.length > 150 && (
-            <TouchableOpacity onPress={() => handleNewsItemPress(item)}>
-              <Text style={styles.readMore}>Read More</Text>
+            <TouchableOpacity
+              onPress={() => handleNewsItemPress(item)}
+              style={{ marginLeft: 10 }}
+            >
+              <ReadMoreButton />
             </TouchableOpacity>
           )}
 
-          {item?.genres && (
-            <Text style={styles.genres}>Genres: {item.genres.join(', ')}</Text>
-          )}
+          {/* <FlatList
+              data={item.genres}
+              horizontal
+              renderItem={({ item }) => (
+                <Genre genre={item} fontRegular={fontRegular} />
+              )}
+              keyExtractor={(item) => item}
+            /> */}
+
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              height: 40,
+              position: 'relative',
+            }}
+          >
+            {item.genres
+              ?.slice(0, Math.min(3, item.genres.length))
+              .map((item, index) => (
+                <View key={index} style={{ position: 'relative' }}>
+                  <Genre genre={item} />
+                  <Text
+                    style={{
+                      color: COLORS.GraySecondary,
+                      fontFamily: 'semi-bold',
+                      position: 'absolute',
+                      top: 5,
+                      left: (item.length * 10) / 2 - item.length * 2,
+                    }}
+                  >
+                    {item}
+                  </Text>
+                </View>
+              ))}
+          </View>
+
           {item?.source && (
             <Text style={styles.source}>Source: {item.source}</Text>
           )}
@@ -280,7 +486,6 @@ const AnimeNewsFeedScreen: React.FC<AnimeNewsFeedScreenProps> = ({
       </TouchableOpacity>
     );
   };
-
   return (
     <View style={{ flex: 1 }}>
       <Canvas
@@ -293,7 +498,7 @@ const AnimeNewsFeedScreen: React.FC<AnimeNewsFeedScreenProps> = ({
       >
         <Vertices vertices={vertices} colors={colors} indices={indices} />
       </Canvas>
-      <RoundedNeumorphicButton />
+
       <Modal
         animationType="slide"
         transparent={true}
@@ -305,7 +510,7 @@ const AnimeNewsFeedScreen: React.FC<AnimeNewsFeedScreenProps> = ({
         <View></View>
       </Modal>
       <TouchableOpacity onPress={() => {}}>
-        <FontAwesome name="filter" size={50} color="white" />
+        <RoundedNeumorphicButton />
       </TouchableOpacity>
 
       {/* <View style={styles.filterContainer}>
@@ -348,14 +553,15 @@ const AnimeNewsFeedScreen: React.FC<AnimeNewsFeedScreenProps> = ({
           placeholder={{ label: 'Select Status', value: 'undefined' }}
         />
       </View> */}
-
-      {/* <FlatList
-        data={newsData}
-        renderItem={renderNewsItem}
-        keyExtractor={(item) => item.id}
-        onEndReached={handleLoadMore}
-        onEndReachedThreshold={0.5}
-      /> */}
+      <View style={{ marginTop: 150 }}>
+        <FlatList
+          data={newsData}
+          renderItem={renderNewsItem}
+          keyExtractor={(item) => item.id}
+          onEndReached={handleLoadMore}
+          onEndReachedThreshold={0.5}
+        />
+      </View>
     </View>
   );
 };
@@ -415,9 +621,11 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   title: {
-    fontSize: 18,
-    fontWeight: 'bold',
+    fontSize: SIZES.h3,
     marginBottom: 5,
+    color: COLORS.white,
+    margin: 10,
+    fontFamily: 'extra-bold',
   },
   readMore: {
     color: COLORS.primary,
